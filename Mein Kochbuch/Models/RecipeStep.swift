db837f5cd518928aa3bbbac262ca01c39ebd309d
Fabian Breitling <83815772+msfb3@users.noreply.github.com>
2025-09-26 23:33:07 +0200

    added RecipeStep Model

--- Mein Kochbuch/Models/RecipeStep.swift
+++ Mein Kochbuch/Models/RecipeStep.swift
@@ -5,3 +5,17 @@
 //  Created by Fabian Breitling on 26.09.25.
 //
 
+import SwiftData
+@Model
+final class RecipeStep {
+    var index: Int
+    var text: String
+    var minutes: Int?
+    
+
+    init(index: Int, text: String, minutes: Int? = nil) {
+        self.index = index
+        self.text = text
+        self.minutes = minutes
+    }
+}
