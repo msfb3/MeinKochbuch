887e8e915cdd9e9dea9b48ddba8a7b83539484ea
Fabian Breitling <83815772+msfb3@users.noreply.github.com>
2025-09-26 23:29:51 +0200

    Added Ingredient Model

--- Mein Kochbuch/Models/Ingredient.swift
+++ Mein Kochbuch/Models/Ingredient.swift
@@ -5,3 +5,22 @@
 //  Created by Fabian Breitling on 26.09.25.
 //
 
+import SwiftData
+
+@Model
+final class Ingredient {
+    var name: String
+    var amount: Double
+    var unit: Unit
+    
+    enum Unit: String, Codable, CaseIterable, Identifiable {
+        case g, kg, ml, l, tsp, tbsp, pcs
+        var id: String { rawValue }
+    }
+    
+    init(name: String, amount: Double, unit: Unit) {
+        self.name = name
+        self.amount = amount
+        self.unit = unit
+    }
+}
