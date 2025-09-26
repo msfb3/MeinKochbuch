9db30c4cbaeab7de4f27dfd7eeafee383646bbff
Fabian Breitling <83815772+msfb3@users.noreply.github.com>
2025-09-26 23:37:07 +0200

    added ShoppingItem Model

--- Mein Kochbuch/Models/ShoppingItem.swift
+++ Mein Kochbuch/Models/ShoppingItem.swift
@@ -5,3 +5,21 @@
 //  Created by Fabian Breitling on 26.09.25.
 //
 
+import SwiftData
+
+@Model
+final class ShoppingItem {
+    @Attribute(.unique) var id: UUID
+    var name: String
+    var amount: Double
+    var unit: Ingredient.Unit?
+    var checked: Bool
+    
+    init(id: UUID, name: String, amount: Double, unit: Ingredient.Unit? = nil, checked: Bool) {
+        self.id = id
+        self.name = name
+        self.amount = amount
+        self.unit = unit
+        self.checked = checked
+    }
+}
